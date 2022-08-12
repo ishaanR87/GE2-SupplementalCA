131ff3843bda96c1f0857ede8f683e88f5ab580a
Ishaan Rawat <i.rawat98@gmail.com>
2022-08-12 15:33:05 +0100

    added follow camera script to follow certain gameobjects

---
+++ GE2/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target.transform);
+	}
+}
