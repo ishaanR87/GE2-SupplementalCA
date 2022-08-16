905c6909d6a5b7653c40002d702c80356802977c
Ishaan Rawat <i.rawat98@gmail.com>
2022-08-16 21:04:53 +0100

    added statemachines scripts (pursue,flee,statemachines,states)

---
+++ GE2/Assets/Scripts/Fighter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    public float health = 10;
+    public float ammo = 10;
+    
+    public GameObject bullet;
+    public GameObject enemy;
+    public TMPro.TextMeshPro text;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {                
+
+    }
+}
