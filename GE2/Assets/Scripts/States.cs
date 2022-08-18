01a4c329a0c73f0140511eba959ce9a6f93b1904
Ishaan Rawat <i.rawat98@gmail.com>
2022-08-18 01:06:38 +0100

    working on battle, removing bullet and health spawner

--- GE2/Assets/Scripts/States.cs
+++ GE2/Assets/Scripts/States.cs
@@ -14,7 +14,7 @@ class PatrolState : State
     {
         if (Vector3.Distance(
             owner.GetComponent<Fighter>().enemy.transform.position,
-            owner.transform.position) < 10)
+            owner.transform.position) < 1000)
         {
             owner.ChangeState(new DefendState());
         }
@@ -119,18 +119,6 @@ public class Alive:State
             owner.SetGlobalState(dead);
             return;
         }
-
-        if (owner.GetComponent<Fighter>().health <= 2)
-        {
-            owner.ChangeState(new FindHealth());
-            return;
-        }
-        
-        if (owner.GetComponent<Fighter>().ammo <= 0)
-        {
-            owner.ChangeState(new FindAmmo());
-            return;
-        }
     }
 }
 
@@ -145,90 +133,4 @@ public class Dead:State
         }
         owner.GetComponent<StateMachine>().enabled = false;        
     }         
-}
-
-public class FindAmmo:State
-{
-    Transform ammo;
-    public override void Enter()
-    {
-        GameObject[] ammos = GameObject.FindGameObjectsWithTag("Ammo");
-        // Find the closest ammo;
-        Transform closest = ammos[0].transform;
-        foreach(GameObject go in ammos)
-        {
-            if (Vector3.Distance(go.transform.position, owner.transform.position) <
-                Vector3.Distance(closest.position, owner.transform.position))
-                {
-                    closest = go.transform;
-                }
-        }
-        ammo = closest;
-        owner.GetComponent<Seek>().targetGameObject = ammo.gameObject;
-        owner.GetComponent<Seek>().enabled = true;
-    }
-
-    public override void Think()
-    {
-        // If the other guy already took tghe ammo
-        if (ammo == null)
-        {
-            owner.ChangeState(new FindAmmo());
-            return;
-        }
-        if (Vector3.Distance(owner.transform.position, ammo.position) < 1)
-        {
-            owner.GetComponent<Fighter>().ammo += 10;
-            owner.RevertToPreviousState();
-            GameObject.Destroy (ammo.gameObject);
-        }
-    }
-
-    public override void Exit()
-    {
-        owner.GetComponent<Seek>().enabled = false;
-    }
-}
-
-public class FindHealth:State
-{
-    Transform health;
-    public override void Enter()
-    {
-        GameObject[] healths = GameObject.FindGameObjectsWithTag("Health");
-        // Find the closest ammo;
-        Transform closest = healths[0].transform;
-        foreach(GameObject go in healths)
-        {
-            if (Vector3.Distance(go.transform.position, owner.transform.position) <
-                Vector3.Distance(closest.position, owner.transform.position))
-                {
-                    closest = go.transform;
-                }
-        }
-        health = closest;
-        owner.GetComponent<Seek>().targetGameObject = health.gameObject;
-        owner.GetComponent<Seek>().enabled = true;
-    }
-
-    public override void Think()
-    {
-        // If the other guy already took the health
-        if (health == null)
-        {
-            owner.ChangeState(new FindHealth());
-            return;
-        }
-        if (Vector3.Distance(owner.transform.position, health.transform.position) < 2)
-        {
-            owner.GetComponent<Fighter>().health += 10;
-            owner.RevertToPreviousState();
-            GameObject.Destroy (health.gameObject);
-        }
-    }
-
-    public override void Exit()
-    {
-        owner.GetComponent<Seek>().enabled = false;
-    }
 }
